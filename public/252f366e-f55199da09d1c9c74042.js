fa1fc052e6934a507e43765e9d8e3f9cf465a076
CoyuyoUTN <herrero_gonza@hotmail.com>
2023-05-22 00:30:30 -0300

    add public file

---
+++ public/252f366e-f55199da09d1c9c74042.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktio_yuyo_mini_fletes=self.webpackChunktio_yuyo_mini_fletes||[]).push([[228],{5434:function(t,n,e){e.d(n,{w7d:function(){return a}});var i=e(4405);function a(t){return(0,i.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-f55199da09d1c9c74042.js.map
